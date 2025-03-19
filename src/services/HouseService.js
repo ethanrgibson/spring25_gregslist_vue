6b9803a4470f9b8d35cf735b838a54945afd2689
ethanrgibson <ethanrgibson33@gmail.com>
2025-03-19 13:52:43 -0600

    getting all houses from api into my app state

--- src/services/HouseService.js
+++ src/services/HouseService.js
@@ -1,11 +1,15 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { House } from "@/models/House.js"
+import { AppState } from "@/AppState.js"
 
 class HouseService {
   async getHouses() {
 
     const response = await api.get('api/houses')
-    logger.log(response.data)
+    const houses = response.data.map(pojo => new House(pojo))
+    AppState.houses = houses
+    logger.log(houses)
 
 
   }
